5121fdf0ced0e1f24135834ae75f633c8104b9c9
Edwin Tumax <edwintumax@gmail.com>
2021-03-06 12:03:57 -0600

    Iniciando proyecto Kalum 2021

---
+++ Models/Role.cs
@@ -0,0 +1,17 @@
+namespace Kalum2021.Models
+{
+    public class Role
+    {
+        public int Id {get;set;}
+        public string Nombre {get;set;}
+        public Role()
+        {
+            
+        }
+        public Role(int Id, string Nombre)
+        {
+            this.Id = Id;
+            this.Nombre = Nombre;
+        }
+    }
+}
---
+++ Models/Teacher.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Kalum2021.Models
+{
+    public class Teacher : Person
+    {
+        private string _employeeId;
+
+
+        public Teacher()
+        {
+
+        }
+
+        public Teacher(string EmployeeId, string FirstName, string LastName, string Email, DateTime Birthday, string Gender, string Phone)
+            : base(FirstName,LastName,Email,Birthday,Gender,Phone)
+        {
+            this.EmployeeId = EmployeeId;
+        }
+
+        public string EmployeeId
+        {
+            get
+            {
+                return _employeeId;
+            }
+            set
+            {
+                _employeeId = value;
+            }
+        }
+    }
+}
